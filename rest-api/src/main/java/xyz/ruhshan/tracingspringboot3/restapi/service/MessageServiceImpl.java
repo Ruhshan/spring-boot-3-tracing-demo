998a8f3378ac267cb39cfbc3f3fce6b2a44a9968
Ruhshan <5312918+Ruhshan@users.noreply.github.com>
2024-02-24 14:43:46 +0600

    Propagate trace context with rest client

--- rest-api/src/main/java/xyz/ruhshan/tracingspringboot3/restapi/service/MessageServiceImpl.java
+++ rest-api/src/main/java/xyz/ruhshan/tracingspringboot3/restapi/service/MessageServiceImpl.java
@@ -1,17 +1,28 @@
 package xyz.ruhshan.tracingspringboot3.restapi.service;
 
-import org.springframework.stereotype.Service;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 import xyz.ruhshan.tracingspringboot3.common.dto.Message;
 
 @Service
 @Slf4j
 public class MessageServiceImpl implements MessageService {
 
+    private final RestClient restClient;
+    public MessageServiceImpl(@Value("${rest-api-2.url}") String restApi2Url, RestClient.Builder restClientBuilder) {
+        this.restClient  = restClientBuilder.baseUrl(restApi2Url)
+            .defaultHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE).build();
+
+    }
+
     @Override
     public void handleMessage(Message message) {
-        log.info("Processing message {}", message);
+        log.info("Handling message {}", message);
+        this.restClient.post().uri( "/process-message").body(message).retrieve();
+
     }
 
 }
