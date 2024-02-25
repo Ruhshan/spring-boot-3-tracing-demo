24bf1cba206cdcec11bd34a66db67a81e84bdc7d
Ruhshan <5312918+Ruhshan@users.noreply.github.com>
2024-02-26 00:15:43 +0600

    Add kafka consumer application

---
+++ kafka-consumer/src/test/java/xyz/ruhshan/tracingspringboot3/kafkaconsumer/KafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ruhshan.tracingspringboot3.kafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
