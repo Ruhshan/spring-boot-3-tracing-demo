fd8ec9ed76ba491db0498e711abeecab20201268
Ruhshan <5312918+Ruhshan@users.noreply.github.com>
2024-05-02 01:11:51 +0600

    Some more refactoring

--- rest-api-2/src/main/java/xyz/ruhshan/tracingspringboot3/restapi2/config/KafkaProducerConfig.java
+++ rest-api-2/src/main/java/xyz/ruhshan/tracingspringboot3/restapi2/config/KafkaProducerConfig.java
@@ -1,28 +1,18 @@
 package xyz.ruhshan.tracingspringboot3.restapi2.config;
 
-import java.util.*;
-import org.apache.kafka.clients.producer.*;
 import org.springframework.context.annotation.*;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.*;
 import xyz.ruhshan.tracingspringboot3.common.dto.*;
 
 @Configuration
 public class KafkaProducerConfig {
     @Bean
-    public KafkaTemplate<String, Message> messageKafkaTemplate(ProducerFactory<String, Message> messageProducerFactory) {
-
-        Map<String, Object> props = new HashMap<>();
-
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-
-        messageProducerFactory.updateConfigs(props);
+    public KafkaTemplate<String, Message> messageKafkaTemplate(
+            ProducerFactory<String, Message> messageProducerFactory) {
 
         KafkaTemplate<String, Message> template = new KafkaTemplate<>(messageProducerFactory);
         template.setObservationEnabled(true);
 
         return template;
-
     }
-
 }
