1602cfe91edca8e99310c36c6d9d081bbb53429e
Ruhshan <5312918+Ruhshan@users.noreply.github.com>
2024-05-02 01:05:42 +0600

    Refactor consumer

--- kafka-consumer/src/main/java/xyz/ruhshan/tracingspringboot3/kafkaconsumer/config/KafkaConsumerConfig.java
+++ kafka-consumer/src/main/java/xyz/ruhshan/tracingspringboot3/kafkaconsumer/config/KafkaConsumerConfig.java
@@ -1,58 +1,27 @@
 package xyz.ruhshan.tracingspringboot3.kafkaconsumer.config;
 
-
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.kafka.annotation.*;
 import org.springframework.kafka.config.*;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.converter.*;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import xyz.ruhshan.tracingspringboot3.common.dto.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableKafka
 public class KafkaConsumerConfig {
 
-    @Value(value = "${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
-
-    private Map<String, Object> consumerConfigs() {
-        final Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-
-        return props;
-    }
-
-    private ConsumerFactory<String, Message> messageConsumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-    }
-
-
-
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Message> messageContainerFactory(
-    ) {
+            ConsumerFactory<String, Message> messageConsumerFactory) {
+
         ConcurrentKafkaListenerContainerFactory<String, Message> factory =
-            new ConcurrentKafkaListenerContainerFactory<>();
+                new ConcurrentKafkaListenerContainerFactory<>();
 
         factory.getContainerProperties().setObservationEnabled(true);
 
-        factory.setConsumerFactory(messageConsumerFactory());
+        factory.setConsumerFactory(messageConsumerFactory);
 
         return factory;
-
     }
 
 }
--- kafka-consumer/src/main/java/xyz/ruhshan/tracingspringboot3/kafkaconsumer/consumer/MessageConsumer.java
+++ kafka-consumer/src/main/java/xyz/ruhshan/tracingspringboot3/kafkaconsumer/consumer/MessageConsumer.java
@@ -2,16 +2,28 @@ package xyz.ruhshan.tracingspringboot3.kafkaconsumer.consumer;
 
 import java.util.*;
 import lombok.extern.slf4j.*;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.kafka.annotation.*;
+import org.springframework.kafka.listener.AbstractConsumerSeekAware;
+import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.messaging.handler.annotation.*;
 import org.springframework.stereotype.*;
 import xyz.ruhshan.tracingspringboot3.common.dto.*;
 
 @Component
 @Slf4j
-public class MessageConsumer {
+public class MessageConsumer extends AbstractConsumerSeekAware {
     @KafkaListener(topics = "topic-message", groupId = "message-consumer-group", containerFactory = "messageContainerFactory")
-    public void consumeMessage(@Payload Message message, @Headers Map<String, Object> allHeader) {
+    public void consumeMessage(@Payload Message message) {
         log.info("Consumed kafka message {}", message);
     }
+
+    @Override
+    public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekAware.ConsumerSeekCallback callback) {
+        assignments.keySet().stream()
+                .filter(partition -> "topic-message".equals(partition.topic()))
+                .forEach(partition -> callback.seekToBeginning("topic-message", partition.partition()));
+
+    }
+
 }
